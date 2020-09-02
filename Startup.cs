287f768a221695b46b906370f473812337cab186
Teerachai Laothong <tlaothong@gmail.com>
2020-09-02 11:01:39 +0700

    Skip https redirection

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,7 @@ namespace demoazureais
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
